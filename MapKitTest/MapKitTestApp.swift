eada9a01e92b0ed05253617715ee2961f849ef67
Jack1eAW <59226187+Jack1eAW@users.noreply.github.com>
2021-10-10 16:20:07 +0300

    Initial Commit

---
+++ MapKitTest/MapKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitTestApp.swift
+//  MapKitTest
+//
+//  Created by Aleksandr Chebotarev on 10.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
